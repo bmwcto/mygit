26bef346d2c5d5fead6c6b7caedb1ac1ab569e77
bmw@kali <wowjoint@gmail.com>
2021-03-16 22:00:02 +0800

    fix lastip files with shell pwd

--- wg-dom-ip.sh
+++ wg-dom-ip.sh
@@ -6,17 +6,18 @@
 ## 2021-03-16 21:12:18 
 
 
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 dom=$(grep -Ev "^$|[#;]" /etc/wireguard/$1.conf|awk '/Endpoint/{print $3}'|cut -d: -f1)
 nsdomip=$(nslookup $dom 114.114.114.114|awk -F '[ ():]+' 'NR==6 {print $2}')
 
-lastIpFile=wg-dom-last-ip-$1
+lastIpFile=$SHELL_FOLDER/wg-dom-last-ip-$1
 lastIp='no_ip'
 
 if test -f "$lastIpFile"
 then
-        lastIp="$(cat wg-dom-last-ip-$1)"   
+        lastIp="$(cat $SHELL_FOLDER/wg-dom-last-ip-$1)"   
 else 
-        echo 'no_ip' > wg-dom-last-ip-$1
+        echo 'no_ip' > $SHELL_FOLDER/wg-dom-last-ip-$1
 fi
 echo "old ip is:[ $lastIp ]"
 currentIp=$nsdomip
@@ -28,6 +29,5 @@ else
         
 sleep 10s
 systemctl restart wg-quick@$1> /dev/null
-echo "$currentIp" > wg-dom-last-ip-$1
+echo "$currentIp" > $SHELL_FOLDER/wg-dom-last-ip-$1
 fi
-
